a0097867c35880e95bbe49b52e8455e494c7151c
Josias Pérez <103588193+JosiasPerJac@users.noreply.github.com>
2025-11-20 09:03:39 -0500

    Initial Commit

---
+++ Lift/LiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftApp.swift
+//  Lift
+//
+//  Created by Josias Pérez on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
